1ec2eebe7b1725e7f2d99a9a53598c77a2f5577f
Brian de Heus <me@brian.jp>
2024-04-18 12:29:37 +0400

    Actually add update script and make it exectuable

---
+++ bin/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ensure the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Switch to 'ito' user to fetch latest updates from the git repository
+su ito -c 'cd ~/live && git fetch origin main'
+
+# Reload the systemd daemon
+systemctl daemon-reload
+
+# Restart the 'ito' service
+systemctl restart ito.service
